b97e02855050759274b894a10a8b13c7c68e6aed
Marr0x <marwa-a@hotmail.nl>
2018-05-04 23:54:28 +0200

    code clained up

--- homework/week4/scatter.js
+++ homework/week4/scatter.js
@@ -8,20 +8,22 @@
 // Source data: https://www.oecd-ilibrary.org/economics/data/main-economic-indicators/international-trade_data-00045-en
 
 
-// 
+// call main function when whole code is loaded
 window.onload = function() {
-	console.log("something");
 	getData();
 
 };
 
 /**
-* 
-*
+* Get the data from an API request and place it in a queue. 
+* When all data is loaded call the function makeScatterplot, to make the scatterplot.
 **/
 function getData(){
+
+	// API request
 	var trade = "https://stats.oecd.org/SDMX-JSON/data/MEI_TRD/XTEXVA01+XTIMVA01+XTNTVA01.AUS+BEL+CAN+CHL+DNK+EST+FIN+FRA+GRC+HUN+ISL+IRL+ITA+JPN+KOR+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR.CXML.A/all?startTime=2015&endTime=2017&dimensionAtObservation=allDimensions";
 
+	// put data in a queue, so the scatterplot will be made after everything is loaded
 	d3.queue()
 	  .defer(d3.request, trade)
 	  .awaitAll(makeScatterplot);
@@ -29,56 +31,62 @@ function getData(){
 
 
 /**
-*
-*
+* This function converts the data from the API request into a JSON, 
+* then extracts all needed data from the JSON and makes a scatterplot form the data.
 **/
 function makeScatterplot(error, response) {
 	if (error) throw error;
-	console.log(response);
 	
-	var dataTrade = JSON.parse(response[0].responseText)
+	// convert data to JSON
+	var dataTrade = JSON.parse(response[0].responseText);
 	
 	console.log(dataTrade);
 	// console.log(dataTrade.dataSets[0].observations["0:0:0:0:0"][0])
 
 	// number of years, countries, variables (import, export, net trade)
-	noYears = 3;
-	noCountries = 29;
-	noVariables = 3;
+	var noYears = dataTrade.structure.dimensions.observation[4].values.length;;
+	var noCountries = dataTrade.structure.dimensions.observation[1].values.length;
+	var noVariables = dataTrade.structure.dimensions.observation[0].values.length;
 
-	// dictionary for the info per year
+	/**
+	* Iterate through the JSON file to get import, export and net value 
+	* for each country for each year. Put info of each county in to separate ditionaries (infoCountry),
+	* put all dicionaries in a list (dataCountry). Put the list in one Dictionary (infoYear).
+	**/
 	var infoYear = {};
+	for (var year = 0; year < noYears; year++){
 
-	// get import, export and net value for each country for each year
-	 for (var year = 0; year < noYears; year++){
+		var dataCountry = [];
+		for (var country = 0; country < noCountries; country++){
 
-	 	// loop per year through the countries, put info in list
-	 	var dataCountry = [];
-	 	for (var country = 0; country < noCountries; country++){
-
-	 		// put info of each country in a dictiionary
-	 		var info = {};
+	 		var infoCountry = {};
 	 		for (var variable = 0; variable < noVariables; variable++){
 
-	 		// get data/variables from each country from the 'first' year
-			info[variable] = (dataTrade.dataSets[0].observations[variable + ":" + country + ":0:0:" + year][0])
-
+	 		// get data/variables from each country from the JSON
+			infoCountry[variable] = (dataTrade.dataSets[0].observations[variable + ":" + country + ":0:0:" + year][0])
 	 		}
-	 		dataCountry.push(info);
+	 		dataCountry.push(infoCountry);
 		}
 		infoYear[year] = dataCountry;
 	}
-	console.log(infoYear)
 
+	// put info of each year in a separate variable
+	var infoYear1 = infoYear[0], infoYear2 = infoYear[1], infoYear3 = infoYear[2];
 
+	// get country names from the JSON
+	var countryName = [];
+	for (var i = 0; i < 29; i++){
+		countryName.push(dataTrade.structure.dimensions.observation[1].values[i].name);
+	}
+
+	console.log(countryName)
 
 	/** 
-	*	make scatterplot
-	*
+	* Make scatterplot
 	**/
 
 	// set dimentions and margins of the graph
-	var margin = {top: 50, right: 50, bottom: 50, left: 50};
+	var margin = {top: 50, right: 50, bottom: 20, left: 50};
 	var width = 800 - margin.left - margin.right;
 	var height = 500 - margin.top - margin.bottom;
 
@@ -91,55 +99,55 @@ function makeScatterplot(error, response) {
 				.append("g")
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	console.log(infoYear[0])
+	// make a tooltip, source: https://bl.ocks.org/alandunning/274bf248fd0f362d64674920e85c1eb7
+	var tooltip = d3.select("body")
+					.append("div")
+					.attr("class", "toolTip");
+	var g = svg.append("g")
+			   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+	// scale-function for the x-axis
 	var xScale = d3.scaleLinear()
 			  .domain([d3.min(infoYear[0], function(d){return d[1]}), d3.max(infoYear[0], function(d){
 						return d[1];
 					})]).nice()
 			  .range([margin.left, width - margin.right]);
 
+	// scale-function for the y-axis
 	var yScale = d3.scaleLinear()
 					.domain([0, d3.max(infoYear[0], function(d){
 						return d[0];
 					})]).nice()
 					.range([height - margin.left, 0]);
 
-
+	// scale-function for the radius of the circles, radius depends on the Net trade
 	var minNet = d3.min(infoYear[0], function(d){return d[2]; });
 	var maxNet = d3.max(infoYear[0], function(d){return d[2]; });
-
 	var rScale = d3.scaleLinear()
 					.domain([minNet, maxNet])
-					.range([2, 7]);
+					.range([2, 8]);
 
-	var minC = d3.min(infoYear[0], function(d){return d[2]; });
-	var maxC = d3.max(infoYear[0], function(d){return d[2]; });
+	// scale-function for the colors of the circles, color depends on the Net trade
+	var cScale = d3.scaleOrdinal(['#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#08519c','#08306b'])
+					.domain([minNet, maxNet]);
 
-	// color
-	var cScale = d3.scaleOrdinal(d3.schemeCategory20)
-					.domain([minC, maxC]);
-
-	var cValue = function(d){ return d[2];}
-	var color = d3.scaleOrdinal(d3.schemeCategory10)
-
-	// add the x axis
+	// draw the x axis
 	var xAxis = svg.append("g")
 					  .attr("transform", "translate(0," + (height - margin.top) + ")")
 					  .call(d3.axisBottom(xScale))
 
-	// text label for the x axis
+	// text label for the x-axis
 	svg.append("text")
 	   .attr("transform", "translate(" + (width/2) + " ," + height + ")")
 	   .style("text-anchor", "middle")
 	   .text("Import");
 
-	// add the y axis
+	// draw the y-axis
 	var yAxis = svg.append("g")
-					.attr("transform", "translate(" + 50 + ", 0)")
+					.attr("transform", "translate(" + margin.left + ", 0)")
 					.call(d3.axisLeft(yScale))
 
-	// text label for the y axis
+	// text label for the y-axis
 	svg.append("text")
 	   .attr("transform", "rotate(-90)")
 	   .attr("y", 0 - margin.left/2)
@@ -148,8 +156,9 @@ function makeScatterplot(error, response) {
 	   .style("text-anchor", "middle")
 	   .text("Export")
 
-	// text title graph
+	// title graph
 	svg.append("text")
+	   .attr("class", "title")
 	   .attr("y", -margin.top/2)
 	   .attr("x", width/2)
 	   .style("text-anchor", "middle")
@@ -170,51 +179,30 @@ function makeScatterplot(error, response) {
 			return rScale(d[2]);
 		})
 		.style("fill", function(d){
-			console.log(d)
 			return (cScale(d[2]));
 		})
 
-
-	//
-	svg.append("text")
-	   .attr("class", "name")
-	   .attr("y", height + 10)
-	   .attr("x", -margin.left)
-	   .style("text-anchor", "left", "8")
-	   .style("front-size", "8px")
-	   .text("Marwa Ahmed, Student number 10747141");
-
-	svg.append("text")
-	   .attr("y", height + 30)
-	   .attr("x", -margin.left)
-	   .style("text-anchor", "left", "8")
-	   .style("front-size", "8px")
-	   .text("Assignment: D3 - Scatterplot");
-
-	var word = "Source: OECD ilibrary - International trade"
-
-	svg.append("a")
-	   .attr("xlink:href", "https://www.oecd-ilibrary.org/economics/data/main-economic-indicators/international-trade_data-00045-en"+word)
-	   .append("text")
-	   .attr("y", height + 50)
-	   .html('<a href = "https://www.oecd-ilibrary.org/economics/data/main-economic-indicators/international-trade_data-00045-en">My link</a>')
-	   .attr("x", -margin.left)
-	   .style("text-anchor", "left", "8")
-	   .style("pointer-events", "none")
-	   .style("fill", "black")
-	   .style("front-size", "8px")
-	   .text(word);
+		// when hovering over the datapoint the name of the county must be shown
+		// use the array 'countryName' 
+		.on("mousemove", function(d){
+	            return tooltip
+	              .style("left", d3.event.pageX - 50 + "px")
+	              .style("top", d3.event.pageY - 50 + "px")
+	              .style("display", "inline-block")
+	              .html((d));
+	        })
+		.on("mouseout", function(d){ tooltip.style("display", "none");});
 
 
-	// svg.append("text")
-	//    .attr("y", height + 45)
-	//    .attr("x", -margin.left)
-	//    .style("text-anchor", "left", "8")
-	//    .style("front-size", "8px")
-	//    .text(word);
+// this part doesnot work yet...
+// source button code: https://www.w3schools.com/howto/howto_js_dropdown.asp
 
-
-onclick="myFunction()"
+// var button = d3.selectAll(".year")
+// 				.on("click", function () {
+//
+//						
+// 				}
+// 			)
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
@@ -226,17 +214,16 @@ function myFunction() {
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var Year = document.getElementsByClassName("dropdown-content");
     var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+    for (i = 0; i < Year.length; i++) {
+      var openDropdown = Year[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
   }
 }
-
 };
 
 
